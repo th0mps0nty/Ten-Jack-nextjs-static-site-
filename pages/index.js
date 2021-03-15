8f238a3e6124bce2d7e8e775297e8d3f50c25590
Tyler <22359488+th0mps0nty@users.noreply.github.com>
2021-03-14 22:41:02 -0400

    added data fetching!

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home({articles}) {
+    console.log(articles)
     return (
         <div>
             <Head>
@@ -14,4 +15,20 @@ export default function Home() {
             <h1>Welcome to Next</h1>
         </div>
     )
+}
+
+// Three separate methods we can use to fetch data with NextJS
+// 1 - getStaticProps - fetch data at buildtime
+// 2 - getServerSideProps - fetch data on each request
+// 3 - getStaticPaths - dynamically generate paths to generate data
+
+export const getStaticProps = async () => {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`)
+    const articles = await res.json();
+
+    return {
+        props: {
+            articles
+        }
+    }
 }
